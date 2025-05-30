74edd05d820b90ba174704d0184ec48ccbb5d66f
Stacy Lee <stacy.lee92@gmail.com>
2025-05-29 18:45:13 -0700

    on number 7

--- submission.sql
+++ submission.sql
@@ -19,3 +19,24 @@ mainframe_override=# SELECT username, password, first_name, last_name FROM empty
 ----------------+-------------+------------+-----------
  triple-cart-38 | password456 | Andrew     | Steele
 (1 row)
+
+
+mainframe_override=# SELECT * FROM emptystack_messages WHERE body ILIKE '%taxi%';
+  id   |     from     |       to       |   subject    |                            body                            
+-------+--------------+----------------+--------------+------------------------------------------------------------
+ LidWj | your-boss-99 | triple-cart-38 | Project TAXI | Deploy Project TAXI by end of week. We need this out ASAP.
+(1 row)
+
+
+mainframe_override=# SELECT username, password, first_name, last_name FROM emptystack_accounts WHERE username = 'your-boss-99';
+   username   |    password    | first_name | last_name 
+--------------+----------------+------------+-----------
+ your-boss-99 | notagaincarter | Skylar     | Singer
+(1 row)
+
+
+mainframe_override=# SELECT id FROM emptystack_projects WHERE code ILIKE '%TAXI%';
+    id    
+----------
+ DczE0v2b
+(1 row)
